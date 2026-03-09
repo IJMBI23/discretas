f81f40ed1b4ee685bd9ddb6fca06b808a5d301f2
Johan Montero <Mj4432Bonte@gmail.com>
2026-03-09 11:13:07 -0600

    Improve bubble sort and add performance metrics
    Refactor bubble sort implementation and add performance analysis.

--- bubble_sort_inst.py
+++ bubble_sort_inst.py
@@ -20,7 +20,8 @@ def bubble_sort_inst(a: list) -> list[int]:
             swaps += 1
 
     n = len(a)
-
+    
+    
     for limit in range(n, 0, -1): #Generates n,n-1,n-2,...,2,1
         for i in range(0, limit - 1): #generates 0,1,...,limit-2
             if a[i] > a[i + 1]:
@@ -31,8 +32,25 @@ def bubble_sort_inst(a: list) -> list[int]:
 a = [666,665,664]
 print(bubble_sort_inst(a))
 
+N = [] #values for n
+SN = [] #seaps for n
+EN = [] # estimates for n
+
 for n in range(5,50,5):
     a = list(range(n, -1, -1))
     _, swaps = bubble_sort_ints(a)
-    la = print(len(a))
-    print(len(a), swaps, la*(la - 1) // 2)
+    la = len(a))
+    N.append(la)
+    SN.append(swaps)
+    EN.append( la*(la - 1) // 2 )
+
+def foo(x, y = 1): # y es un valor opcional
+    return x / y
+
+print(foo(10, 20))
+print(foo(y = 20, x = 10))
+print(foo(10))
+    
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(5,6))
+
