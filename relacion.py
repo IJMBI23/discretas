f738351a346073b9b11623b4d12e8e8a3a3e285e
Johan Montero <Mj4432Bonte@gmail.com>
2026-04-09 11:11:27 -0600

    Update relacion.py

--- relacion.py
+++ relacion.py
@@ -10,18 +10,27 @@ AxB = {(a,b) for a in A for b in B}
 print (AxB)
 
 #Relacion como objeto
-#Relacion como objeto
 class Relation:
     """
     An OOP representation of a mathematical relation
     """
-    def __init__(self, A, B = None):
+    def __init__(self, A, B = None, pairs = []):
         self.A = A
         self.B = B if B else A
+        self.pairs = {p for p in pairs}
+
+    def __contains__(self, pair):
+        return pair in self.pairs
 
     def __str__(self):
-        return f"Relation( {self.A}, {self.B})"
+        return f"Relation( {self.A}, {self.B}, {self.pairs})"
+
+    def __len__(self):
+        return len(self.pairs)
 
 
-R = Relation({"a", "b"})
+R = Relation({"a", "b"}, pairs=[("a", "a"), ("a", "b"), ("b", "b")])
+
+print(("a", "b") in R)
+print(len(R))
 
